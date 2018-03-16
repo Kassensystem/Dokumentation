80cd8670e5719cfa268b82f13dfadcce2ae66ed5
marvinmai <marvinmai@posteo.de>
2018-03-16 13:26:37 +0100

    falsche Datenbank exportiert, neu erzeugt

--- Database Export/Dump20180316.sql
+++ Database Export/Dump20180316.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: homeautomation
+-- Host: 127.0.0.1    Database: kassensystem
 -- ------------------------------------------------------
 -- Server version	5.7.19-log
 
@@ -16,20 +16,115 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `roomclimate`
+-- Table structure for table `itemdeliveries`
 --
 
-DROP TABLE IF EXISTS `roomclimate`;
+DROP TABLE IF EXISTS `itemdeliveries`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `roomclimate` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `timestamp` datetime DEFAULT NULL,
-  `temperature` float DEFAULT NULL,
-  `humidity` float DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `roomclimate_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+CREATE TABLE `itemdeliveries` (
+  `itemdeliveryID` int(11) NOT NULL AUTO_INCREMENT,
+  `itemID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`itemdeliveryID`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `itemID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(30) NOT NULL,
+  `retailprice` float DEFAULT NULL,
+  `available` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`itemID`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `logindata`
+--
+
+DROP TABLE IF EXISTS `logindata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logindata` (
+  `waiterID` int(11) NOT NULL,
+  `loginname` char(20) NOT NULL,
+  `passwordhash` char(200) NOT NULL,
+  UNIQUE KEY `logindata_waiterID_uindex` (`waiterID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ordereditems`
+--
+
+DROP TABLE IF EXISTS `ordereditems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ordereditems` (
+  `orderedItemID` int(11) NOT NULL AUTO_INCREMENT,
+  `orderID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  `itemPaid` tinyint(1) DEFAULT '0',
+  `itemProduced` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`orderedItemID`)
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `orderID` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `tableID` int(11) NOT NULL,
+  `waiterID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`orderID`)
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tables`
+--
+
+DROP TABLE IF EXISTS `tables`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tables` (
+  `tableID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(30) NOT NULL,
+  `seats` int(11) DEFAULT '0',
+  `available` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`tableID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `waiters`
+--
+
+DROP TABLE IF EXISTS `waiters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `waiters` (
+  `waiterID` int(11) NOT NULL AUTO_INCREMENT,
+  `lastname` char(20) DEFAULT NULL,
+  `prename` char(20) DEFAULT NULL,
+  `employed` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`waiterID`),
+  UNIQUE KEY `waiters_waiterID_uindex` (`waiterID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -41,4 +136,4 @@ CREATE TABLE `roomclimate` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-16 12:46:57
+-- Dump completed on 2018-03-16 13:25:10
